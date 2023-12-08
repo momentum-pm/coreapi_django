bf3f9ef9dcf2425dc4d458580fcfb76814272677
reza <rmoslemi1994@gmail.com>
2023-12-09 00:54:34 +0330

    changed

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ source .venv/bin/activate
 python manage.py migrate
 deactivate
 mkdir /run/uwsgi/
-mkdir /run/uwsgi/coreapi_django/
-rm -f /run/uwsgi/coreapi_django/coreapi_django.sock
+mkdir /run/uwsgi/coreapi/
+rm -f /run/uwsgi/coreapi/coreapi_django.sock
 mkdir /var/log/uwsgi/
 touch /var/log/uwsgi/coreapi_django.log
 sudo uwsgi --ini uwsgi_config.ini
