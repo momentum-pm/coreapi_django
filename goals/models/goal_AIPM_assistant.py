ae9d0df11dc95725600357ccc8c4f0fee5702307
reza <rmoslemi1994@gmail.com>
2023-12-10 16:47:05 +0330

    comments

--- goals/models/goal_AIPM_assistant.py
+++ goals/models/goal_AIPM_assistant.py
@@ -12,7 +12,7 @@ class GoalAIPMAssistant(Assistant):
         return f"{self.goal.name} Goal AI PM Assistant"
 
     def _create_context_str(self):
-        goal_context = '''
+        goal_context = """
                         Each goal has a owner which is a person. Also each goal as an AI PM (artificial Intelligence project manager). Whenever new information comes in from assistants, Systems, Other PMs, or the owner, the PM will synthesize the info, find out what this info means, what actions it should take, and bring the actions to the owner. If the owner confirms, it does the actions.
                         Each goal also can have some subgoals. Goals (or subgoals with together) can be depenedant to each other. They can be parent/child. For example each goal is a parent for its subgoals.
                         Here are some events that can lead to the updates in a system designed in this architecture:
@@ -47,28 +47,32 @@ class GoalAIPMAssistant(Assistant):
                         Project TimeLine: {goal_project_timeline}
                         --------------------------------------------------------------------------------------------------------------------------------
                         People Tasks: {goal_people_tasks}
-                       '''.format(
-                           goal_name = self.goal.name,
-                           goal_summary = self.goal.summary,
-                           goal_key_metrics = self.goal.goal_key_metrics,
-                           goal_latest_actions = self.goal.goal_latest_actions,
-                           goal_related_people = self.goal.people,
-                           goal_subgoals = self.goal.subgoals,
-                           goal_entities = self.goal.Entities,
-                           goal_risks = self.goal.risks,
-                           goal_dependencies = self.goal.Dependencies,
-                           goal_execution_plan = self.goal.goal_execution_plan,
-                           goal_project_timeline = self.goal.timeline,
-                           goal_people_tasks = self.goal.tasks
-                       )
+                       """.format(
+            goal_name=self.goal.name,
+            goal_summary=self.goal.summary,
+            goal_key_metrics=self.goal.goal_key_metrics,
+            goal_latest_actions=self.goal.goal_latest_actions,
+            goal_related_people=self.goal.people,
+            goal_subgoals=self.goal.subgoals,
+            goal_entities=self.goal.Entities,
+            goal_risks=self.goal.risks,
+            goal_dependencies=self.goal.Dependencies,
+            goal_execution_plan=self.goal.goal_execution_plan,
+            goal_project_timeline=self.goal.timeline,
+            goal_people_tasks=self.goal.tasks,
+        )
         return goal_context
-    
+
     def get_default_instructions(self):
-        default_instructions = '''
+        default_instructions = """
                You are the project manager of this goal: {goal_name} 
-               '''.format(self.goal.name)
-        default_instructions += '''Context is: {context}'''.format(context=self._create_context_str())
-        return 
+               """.format(
+            self.goal.name
+        )
+        default_instructions += """Context is: {context}""".format(
+            context=self._create_context_str()
+        )
+        return
 
     def get_instructions_for_run(self, member):
         unseen_notifications = self.goal.notifications.filter(is_seen=False)
@@ -114,7 +118,9 @@ class GoalAIPMAssistant(Assistant):
                     You should list all of changes and updates should be applied in the system based on the new information and events user will gave you.
                     The new information will be given in messages.
                     Handle the project carefully. You are the project-manager.
-                    Any new information come to you specify any change in all of the variables of the system specially the execution plan and timeline of the project.""".format(goal_name=self.goal.name),
+                    Any new information come to you specify any change in all of the variables of the system specially the execution plan and timeline of the project.""".format(
+                    goal_name=self.goal.name
+                ),
                 "parameters": {
                     "type": "object",
                     "properties": {
@@ -128,3 +134,39 @@ class GoalAIPMAssistant(Assistant):
                 },
             },
         )
+
+
+"""
+json1: GoalPreDefineSerializer
+{
+id,
+name,
+summary,
+metrics: ["metric","defenition"]
+subgoals: ["id","name","summary"]
+dependcies: ["id","name","summary"]
+}
+
+"""
+
+
+"""
+json2: GoalThreadSerializer
+{
+
+start,
+end,
+responsibilites: ["id","summary","people":["name","about","id"],"lastest_action","state"]
+latest_metrics_states: ["metric","value","action"]
+subgoals: ["id","start","end","state"]
+dependcies: ["id","start","end","state"]
+
+python dictionary
+}
+"""
+
+
+
+"""
+
+"""
